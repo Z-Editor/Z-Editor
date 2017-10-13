58810ba43d9d1ae0abe12d7dcfc4840368848eaa
anzcarroll <anzcarroll@gmail.com>
2017-10-13 15:13:20 -0400

    tried to uncouple topbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React, { Component } from 'react';
-import ZEditor from './editor/editor'
+import ZEditor from './editor/editor';
+import TopBar from './TopBar';
 import './App.css';
 
 class App extends Component {
-  
+
     render() {
         return (
             <div className="flex-container">
-              <div className="head">
-                    <div className="logo">Z-Editor</div>
-                    <a className="github-button" href="https://github.com/Z-Editor/Z-Editor" target="_blank">
-                        View on Github
-                    </a>
-                    <button className="button" onClick={() => {window.print()}}>
-                        Print
-                    </button>
-                </div>
-                
-                <ZEditor/>
-                
+
+                <TopBar />
+
+                <ZEditor />
+
             </div>
         );
     }
