bb50948ecd827bcbebe9f31a725f9f513b7f0f8b
Nadun Indunil <nadun1indunil@gmail.com>
2018-10-08 19:45:06 +0530

    add: new prettier settings

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
 import React, { Component } from 'react';
-import ZEditor from './editor/editor'
+import ZEditor from './editor/editor';
 import './App.css';
 
 class App extends Component {
-  
-    render() {
-        return (
-            <div className="flex-container">
-              <div className="head">
-                    <div className="logo">Z-Editor</div>
-                    <a className="github-button" href="https://github.com/Z-Editor/Z-Editor" target="_blank">
-                        View on Github
-                    </a>
-                    <button className="button" onClick={() => {window.print()}}>
-                        Print
-                    </button>
-                </div>
-                
-                <ZEditor/>
-                
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="flex-container">
+        <div className="head">
+          <div className="logo">Z-Editor</div>
+          <a className="github-button" href="https://github.com/Z-Editor/Z-Editor" target="_blank">
+            View on Github
+          </a>
+          <button
+            className="button"
+            onClick={() => {
+              window.print();
+            }}
+          >
+            Print
+          </button>
+        </div>
+
+        <ZEditor />
+      </div>
+    );
+  }
 }
 
 export default App;
