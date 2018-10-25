5dd0a8c23bfd0ecdb93b426c8511c64465736eb3
Dulaj Atapattu <dulaj.r.atapattu@gmail.com>
2018-10-25 22:51:32 +0530

    Added unit tests for SideToolBar component (#24)
    * Ignored IntelliJ IDE files from git
    
    * <SideToolBar> covered with unit tests
    
    * Removing unused codes

---
+++ src/__mocks__/config.mock.js
@@ -0,0 +1,11 @@
+import full from '../images/full.svg';
+import half from '../images/half.svg';
+import inverse from '../images/inverse.svg';
+import bar from '../images/bar.svg';
+
+export const l_config = [
+  { id: 100, type: 'main', icon: full },
+  { id: 101, type: 'half', icon: half },
+  { id: 102, type: 'bar', icon: bar },
+  { id: 103, type: 'inverse', icon: inverse }
+];
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import App from './App';
+import App from '../App';
 
 configure({ adapter: new Adapter() });
 
