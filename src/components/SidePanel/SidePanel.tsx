c8264f8a117c969bddc56431f31193cad0c102c1
nadunindunil <nadun1indunil@gmail.com>
2025-01-28 20:07:31 +1100

    feat: Add collapsible side panel

--- src/components/SidePanel/SidePanel.tsx
+++ src/components/SidePanel/SidePanel.tsx
@@ -1,7 +1,7 @@
 import './SidePanel.css';
 
 import { EditorState } from 'prosemirror-state';
-import { ComponentType } from 'react';
+import { ComponentType, useState } from 'react';
 
 import { Handle } from '../Editor/Editor';
 import { sideBarSymbols } from './constants';
@@ -13,6 +13,8 @@ interface SidePanelProps {
 }
 
 const SidePanel: ComponentType<SidePanelProps> = ({ editorState, setEditorState, editorRef }) => {
+  const [shouldCollapse, setCollapse] = useState<boolean>(true);
+
   const handleSymbolClick = (symbol: string) => {
     const { tr, selection } = editorState;
     const { from, to } = selection;
@@ -22,18 +24,36 @@ const SidePanel: ComponentType<SidePanelProps> = ({ editorState, setEditorState,
     editorRef.current?.view?.focus();
   };
 
+  const toggleCollapse = () => {
+    setCollapse(!shouldCollapse);
+  };
+
   return (
-    <div className="side-panel">
-      {sideBarSymbols.map((section, index) => (
-        <div className="symbol-section" key={index}>
-          {section.data.map((symbol, index) => (
-            <div className="symbol" key={index} onClick={() => handleSymbolClick(symbol.symbol)}>
-              {symbol.symbol}
+    <>
+      {shouldCollapse ? (
+        <div className="collapsed-side-bar">
+          <button className="toggle-collapse-button" onClick={toggleCollapse}>
+            {'<<'}
+          </button>
+        </div>
+      ) : (
+        <div className="side-panel">
+          <button className="toggle-collapse-button" onClick={toggleCollapse}>
+            {'>>'}
+          </button>
+
+          {sideBarSymbols.map((section, index) => (
+            <div className="symbol-section" key={index}>
+              {section.data.map((symbol, index) => (
+                <div className="symbol" key={index} onClick={() => handleSymbolClick(symbol.symbol)}>
+                  {symbol.symbol}
+                </div>
+              ))}
             </div>
           ))}
         </div>
-      ))}
-    </div>
+      )}
+    </>
   );
 };
 
